7f66704e14dd1fc52767a43cbbdba23c05f9a367
Luis Miguel Martins Mineiro <l.martins.mineiro@campus.tu-berlin.de>
2025-12-05 09:20:55 +0100

    Finished Day02

--- day02.c
+++ day02.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 //-----------------------Help Functions------------------------------
-bool is_invalid_id(uint64_t num) {
+bool inv_ID_1(uint64_t num) {
     char str[32];
     sprintf(str, "%llu", num);
     
@@ -27,6 +27,34 @@ bool is_invalid_id(uint64_t num) {
     
     return true;
 }
+
+bool inv_ID_2(uint64_t num) {
+    char str[32];
+    sprintf(str, "%llu", num);
+    int len = strlen(str);
+
+    if (len < 2) {
+        return false;
+    }
+
+    for (int pattern_len = 1; pattern_len <= len / 2; pattern_len++) {
+        if (len % pattern_len != 0) {
+            continue;
+        }
+
+        bool repeats = true;
+        for (int i = pattern_len; i < len; i++) {
+            if (str[i] != str[i % pattern_len]) {
+                repeats = false; 
+                break;
+            }
+        }
+        if (repeats) {
+            return true;
+        }
+    }
+    return false;
+} 
 // ------------------------------------------------------------------
 
 
@@ -55,7 +83,7 @@ void solve_part1(int day) {
         printf("Range: %llu to %llu\n", first_val, last_val);
 
         for (uint64_t num = first_val; num <= last_val; num++) {
-            if (is_invalid_id(num)) {
+            if (inv_ID_1(num)) {
                 printf("Invalid ID: %llu\n", num);
                 total += num;
             }
@@ -84,6 +112,35 @@ void solve_part2(int day) {
     printf("Part 2 Solution:\n");
     
 // -----------------------Solution Here------------------------------ 
+    const char *p = input;
+    uint64_t total = 0;
+    
+    while (*p != '\0') {
+        uint64_t first_val, last_val;
+        char *next;
+        
+        first_val = strtoull(p, &next, 10);
+        if (*next != '-') break;
+        
+        p = next + 1;
+        last_val = strtoull(p, &next, 10);
+        
+        printf("Range: %llu to %llu\n", first_val, last_val);
+
+        for (uint64_t num = first_val; num <= last_val; num++) {
+            if (inv_ID_2(num)) {
+                printf("Invalid ID: %llu\n", num);
+                total += num;
+            }
+        }
+            
+        
+        p = next;
+        while (*p == ',' || *p == ' ' || *p == '\n') {
+            p++;
+        }
+    }
+    printf("Total sum of invalid IDs: %llu\n", total);
 
 // ------------------------------------------------------------------    
 
